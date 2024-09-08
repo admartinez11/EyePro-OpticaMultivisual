5794bd00d0df53a78883bcb528eaf5129f01d239
Amurgas31 <20240235@ricaldone.edu.sv>
2024-09-08 15:32:05 -0600

    Configuración del server

---
+++ Views/Server/ViewConfirmPassword.cs
@@ -0,0 +1,22 @@
+﻿using OpticaMultivisual.Controllers.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpticaMultivisual.Views.Server
+{
+    public partial class ViewConfirmPassword : Form
+    {
+        public ViewConfirmPassword(string usuario)
+        {
+            InitializeComponent();
+            ControllerConfirmPassword objControl = new ControllerConfirmPassword(this, usuario);
+        }
+    }
+}
