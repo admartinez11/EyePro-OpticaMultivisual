0293d180cde588ce539c85483da172cc2848a916
admartinez11 <20210130@ricaldone.edu.sv>
2024-09-15 12:48:37 -0600

    Corecciones del 80. Adriana

--- Models/DAO/DAO_DR.cs
+++ Models/DAO/DAO_DR.cs
@@ -54,13 +54,13 @@ namespace OpticaMultivisual.Models.DAO
             {
                 Command.Connection = getConnection();
                 //Definir instrucción de lo que se quiere hacer
-                string query = "SELECT con_ID FROM Consulta";
+                string query = "SELECT cli_DUI FROM Cliente";
                 //Creando un objeto de tipo comando donde recibe la instrucción y la conexión
                 SqlCommand cmdSelect = new SqlCommand(query, Command.Connection);
                 cmdSelect.ExecuteNonQuery();
                 SqlDataAdapter adp = new SqlDataAdapter(cmdSelect);
                 DataSet ds = new DataSet();
-                adp.Fill(ds, "Consulta");
+                adp.Fill(ds, "Cliente");
                 return ds;
             }
             catch (Exception)
