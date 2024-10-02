fb6bb827ff7f4a9c692cbd3dc4609ccaf1ba5984
Amurgas31 <20240235@ricaldone.edu.sv>
2024-10-01 20:10:58 -0600

    ???

---
+++ Controllers/Article/Material/ControllerAdminMaterial.cs
@@ -0,0 +1,20 @@
+﻿using OpticaMultivisual.Models.DAO;
+using OpticaMultivisual.Views.Dashboard.Article.Material;
+using OpticaMultivisual.Views.Dashboard.Article.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpticaMultivisual.Controllers.Article.Material
+{
+    internal class ControllerAdminMaterial
+    {
+        ViewAdminMaterial ObjVistaR;
+        private int accion;
+        protected int Id;
+    }
+}
